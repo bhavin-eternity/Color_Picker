2b5650fcec3022aa1fba8aba350cb2c6e1a3dc1d
Mr Robot <robot@example.com>
2026-05-10 21:47:34 +0530

    SLider component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import './App.css';
 
-import Colorpicker from './Colorpicker'
+function toHex(n:number):string{
+  return n.toString(16).padStart(2,'0')
+}
 
-function App() {
-  return (
-    <div>
-      <Colorpicker/>
+type SliderProps ={
+  label: string
+  color:string
+  value:number
+  onChange: (n:number)=> void
+}
+
+function Slider({label,color,value,onChange}:SliderProps){
+  return(
+    <div className="slider-group">
+      <div className="slider-header">
+        <span style={{color}}>{label}</span>
+        <span>{value}</span>
+      </div>
+      <input type="range" value={value} min={0} max={255} onChange={e=>onChange(Number(e.target.value))}/>
     </div>
   )
 }
 
-export default App
+type SwatchProps ={hex: string;onClick:()=>void}
+
+function Swatch(hex,onClick:SwatchProps){
+return(
+  <button className="swatch-dot" style={{background:hex}} onClick={onclick}/>
+    
+  
+)
+}
+
+type SavedColor={
+  r:number
+  g:number
+  b:number
+  hex:string
+}
